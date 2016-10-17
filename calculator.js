35ad94dbb9e832d441b9039ef2f0b7cebf106af9
greenitthe <greenitthe@gmail.com>
2016-10-17 18:58:21 -0500

    May or may not work nicely on mobile
    Def. miiiiight not though. So sad.
    :(

--- calculator.js
+++ calculator.js
@@ -120,7 +120,6 @@ for(var i = 0; i < keys.length; i++) {
 		var inputVal = input.textContent;
 		var btnVal = this.textContent;
 
-		// Now, just append the key values (btnValue) to the input string and finally use javascript's eval function to get the result
 		// If clear key is pressed, erase everything
 		if(btnVal == 'C') {
 			input.textContent = '';
@@ -132,7 +131,6 @@ for(var i = 0; i < keys.length; i++) {
 			var equation = inputVal;
 			var lastChar = equation[equation.length - 1];
 
-			// Final thing left to do is checking the last character of the equation. If it's an operator or a decimal, remove it
 			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
 				equation = equation.replace(/.$/, '');
 
@@ -144,27 +142,21 @@ for(var i = 0; i < keys.length; i++) {
 
 		else if(operators.indexOf(btnVal) > -1) {
 			// Operator is clicked
-			// Get the last character from the equation
 			var lastChar = inputVal[inputVal.length - 1];
 
-			// Only add operator if input is not empty and there is no operator at the last
 			if(inputVal != '' && operators.indexOf(lastChar) == -1)
 				input.textContent += btnVal;
 
-			// Allow minus if the string is empty
 			else if(inputVal == '' && btnVal == '-')
 				input.textContent += btnVal;
 
-			// Replace the last operator (if exists) with the newly pressed operator
 			if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
-				// Here, '.' matches any character while $ denotes the end of string, so anything (will be an operator in this case) at the end of string will get replaced by new operator
 				input.textContent = inputVal.replace(/.$/, btnVal);
 			}
 
 			decimalAdded =false;
 		}
 
-		// Now only the decimal problem is left. We can solve it easily using a flag 'decimalAdded' which we'll set once the decimal is added and prevent more decimals to be added once it's set. It will be reset when an operator, eval or clear key is pressed.
 		else if(btnVal == '.') {
 			if(!decimalAdded) {
 				input.textContent += btnVal;
@@ -172,12 +164,10 @@ for(var i = 0; i < keys.length; i++) {
 			}
 		}
 
-		// if any other key is pressed, just append it
 		else {
 			input.textContent += btnVal;
 		}
 
-		// prevent page jumps
 		e.preventDefault();
 	}
 }
